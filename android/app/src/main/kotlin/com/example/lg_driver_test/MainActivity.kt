944d5f3d2a341a7c71cb1782793127985bb10af5
youksimgyu <dbrtlarb@gmail.com>
2024-01-22 20:23:30 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lg_driver_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lg_driver_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lg_driver_test/widgets/home_screen.dart';
+
+void main() {
+  runApp(const LGDriverTest());
+}
+
+class LGDriverTest extends StatelessWidget {
+  const LGDriverTest({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LG Driver Test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(title: 'LG Driver Test'),
+    );
+  }
+}
